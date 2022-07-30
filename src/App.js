a19cee378782f2435daa37d4e3fa5ca81300923a
DESKTOP-TMB22QD\balin <balint97@yahoo.com>
2022-07-30 09:59:29 +0300

    Added project files

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// import logo from "./logo.svg";
+import { Component } from "react";
+import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.componenet";
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: "",
+		};
+	}
+
+	onSearchValue = (e) => {
+		this.setState({ searchField: e.target.value });
+	};
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					placeholder="search monsters"
+					handleChange={this.onSearchValue}
+				/>
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
+}
+
+export default App;
